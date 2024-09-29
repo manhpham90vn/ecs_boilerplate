8e0206d4480a38c650198897b6df68732d64ece0
Manh Pham <manhpham90vn@icloud.com>
2024-09-29 15:03:47 +0700

    init

--- lib/ecs_boilerplate-stack.ts
+++ lib/ecs_boilerplate-stack.ts
@@ -1,16 +1,257 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
 
 export class EcsBoilerplateStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const proj = process.env.PROJECT_NAME;
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'EcsBoilerplateQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create vpc
+    const vpc = new cdk.aws_ec2.Vpc(this, "VPC", {
+      vpcName: `${proj}-VPC`,
+      ipAddresses: cdk.aws_ec2.IpAddresses.cidr("10.0.0.0/16"),
+      enableDnsHostnames: true,
+      enableDnsSupport: true,
+      maxAzs: 3,
+      natGateways: 0,
+      subnetConfiguration: [],
+    });
+
+    const availabilityZones = vpc.availabilityZones;
+
+    // Create public subnets
+    const publicSubnets: cdk.aws_ec2.Subnet[] = [];
+    for (let i = 0; i < availabilityZones.length; i++) {
+      const id = `Public-Subnet-${i + 1}`;
+      const cidrBlock = `10.0.${16 * i}.0/20`;
+      const availabilityZone = availabilityZones[i];
+      const publicSubnet = new cdk.aws_ec2.Subnet(this, id, {
+        vpcId: vpc.vpcId,
+        cidrBlock: cidrBlock,
+        availabilityZone: availabilityZone,
+        mapPublicIpOnLaunch: true,
+      });
+      cdk.Tags.of(publicSubnet).add("Name", `${proj}_${id}`);
+      cdk.Tags.of(publicSubnet).add("aws-cdk:subnet-type", "Public");
+      publicSubnets.push(publicSubnet);
+    }
+
+    // Create private subnets
+    const privateSubnets: cdk.aws_ec2.Subnet[] = [];
+    for (let i = 0; i < availabilityZones.length; i++) {
+      const id = `Private-Subnet-${i + 1}`;
+      const cidrBlock = `10.0.${16 * i + 48}.0/20`;
+      const availabilityZone = availabilityZones[i];
+      const privateSubnet = new cdk.aws_ec2.Subnet(this, id, {
+        vpcId: vpc.vpcId,
+        cidrBlock: cidrBlock,
+        availabilityZone: availabilityZone,
+      });
+      cdk.Tags.of(privateSubnet).add("Name", `${proj}_${id}`);
+      privateSubnets.push(privateSubnet);
+    }
+
+    // Create Internet Gateway
+    const igw = new cdk.aws_ec2.CfnInternetGateway(this, "InternetGateway", {
+      tags: [{ key: "Name", value: `${proj}-InternetGateway` }],
+    });
+
+    // Attach Internet Gateway to the VPC
+    new cdk.aws_ec2.CfnVPCGatewayAttachment(this, "VPCGatewayAttachment", {
+      vpcId: vpc.vpcId,
+      internetGatewayId: igw.ref,
+    });
+
+    // Create route table for public subnets
+    const publicRouteTable = new cdk.aws_ec2.CfnRouteTable(
+      this,
+      "PublicRouteTable",
+      {
+        vpcId: vpc.vpcId,
+        tags: [{ key: "Name", value: `${proj}-PublicRouteTable` }],
+      }
+    );
+
+    // Add default route to Internet Gateway in the public route table
+    new cdk.aws_ec2.CfnRoute(this, "DefaultRoute", {
+      routeTableId: publicRouteTable.ref,
+      destinationCidrBlock: "0.0.0.0/0",
+      gatewayId: igw.ref,
+    });
+
+    // Associate public subnets with the public route table
+    publicSubnets.forEach((subnet, index) => {
+      new cdk.aws_ec2.CfnSubnetRouteTableAssociation(
+        this,
+        `PublicSubnetAssociation-${index}`,
+        {
+          subnetId: subnet.subnetId,
+          routeTableId: publicRouteTable.ref,
+        }
+      );
+    });
+
+    // Create security group for VPC Endpoint
+    const vpcEndpointSecurityGroup = new cdk.aws_ec2.SecurityGroup(
+      this,
+      "VPCEndpointSG",
+      {
+        vpc,
+        allowAllOutbound: true,
+      }
+    );
+
+    // Create PrivateLink endpoint for ECR
+    new cdk.aws_ec2.CfnVPCEndpoint(this, "ECREndpoint", {
+      serviceName: `com.amazonaws.${process.env.CDK_DEFAULT_REGION}.ecr.dkr`,
+      vpcId: vpc.vpcId,
+      privateDnsEnabled: false,
+      vpcEndpointType: "Interface",
+      subnetIds: publicSubnets.map((subnet) => subnet.subnetId),
+      securityGroupIds: [vpcEndpointSecurityGroup.securityGroupId],
+    });
+
+    // Create PrivateLink endpoint for ECR API
+    new cdk.aws_ec2.CfnVPCEndpoint(this, "ECRApiEndpoint", {
+      serviceName: `com.amazonaws.${process.env.CDK_DEFAULT_REGION}.ecr.api`,
+      vpcId: vpc.vpcId,
+      privateDnsEnabled: false,
+      vpcEndpointType: "Interface",
+      subnetIds: publicSubnets.map((subnet) => subnet.subnetId),
+      securityGroupIds: [vpcEndpointSecurityGroup.securityGroupId],
+    });
+
+    // Create ECR repository
+    const ecr = new cdk.aws_ecr.Repository(this, "ECR", {
+      repositoryName: `${proj}-ecr`,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    // Create ECS cluster
+    const cluster = new cdk.aws_ecs.Cluster(this, "Cluster", {
+      clusterName: `${proj}-cluster`,
+      vpc: vpc,
+    });
+
+    // Create task definition
+    const taskDefinition = new cdk.aws_ecs.FargateTaskDefinition(
+      this,
+      "TaskDefinition",
+      {
+        cpu: 256,
+        memoryLimitMiB: 512,
+      }
+    );
+
+    // Add container to task definition
+    taskDefinition.addContainer("Container", {
+      image: cdk.aws_ecs.ContainerImage.fromEcrRepository(ecr, "1"),
+      memoryLimitMiB: 512,
+      cpu: 256,
+      portMappings: [{ containerPort: 80 }],
+    });
+
+    // Create Application Load Balancer
+    const alb = new cdk.aws_elasticloadbalancingv2.ApplicationLoadBalancer(
+      this,
+      "ALB",
+      {
+        vpc: vpc,
+        internetFacing: true,
+        loadBalancerName: `${proj}-ALB`,
+        vpcSubnets: {
+          subnets: publicSubnets.map((subnet) => subnet),
+        },
+      }
+    );
+
+    // Create a security group for ALB
+    const albSecurityGroup = new cdk.aws_ec2.SecurityGroup(
+      this,
+      "ALBSecurityGroup",
+      {
+        vpc,
+        allowAllOutbound: true,
+      }
+    );
+
+    albSecurityGroup.addIngressRule(
+      cdk.aws_ec2.Peer.anyIpv4(),
+      cdk.aws_ec2.Port.tcp(80)
+    );
+
+    alb.addSecurityGroup(albSecurityGroup);
+
+    // Create listener for ALB
+    const listener = alb.addListener("Listener", {
+      port: 80,
+    });
+
+    // Create Target Group
+    const targetGroup =
+      new cdk.aws_elasticloadbalancingv2.ApplicationTargetGroup(
+        this,
+        "TargetGroup",
+        {
+          vpc: vpc,
+          port: 80,
+          protocol: cdk.aws_elasticloadbalancingv2.ApplicationProtocol.HTTP,
+          targetType: cdk.aws_elasticloadbalancingv2.TargetType.IP,
+          healthCheck: {
+            path: "/",
+            interval: cdk.Duration.seconds(30),
+          },
+        }
+      );
+
+    // Add target group to listener
+    listener.addTargetGroups("TargetGroups", {
+      targetGroups: [targetGroup],
+    });
+
+    // Create a Security Group for the Fargate Service that allows all IPs within the VPC
+    const serviceSecurityGroup = new cdk.aws_ec2.SecurityGroup(
+      this,
+      "ServiceSecurityGroup",
+      {
+        vpc,
+        allowAllOutbound: true,
+      }
+    );
+
+    // Allow all inbound traffic from within the VPC
+    serviceSecurityGroup.addIngressRule(
+      cdk.aws_ec2.Peer.ipv4(vpc.vpcCidrBlock),
+      cdk.aws_ec2.Port.allTraffic()
+    );
+
+    // Create service
+    const service = new cdk.aws_ecs.FargateService(this, "Service", {
+      cluster: cluster,
+      taskDefinition: taskDefinition,
+      desiredCount: 1,
+      serviceName: `${proj}_Service`,
+      deploymentController: {
+        type: cdk.aws_ecs.DeploymentControllerType.CODE_DEPLOY,
+      },
+      assignPublicIp: false,
+      securityGroups: [serviceSecurityGroup],
+      vpcSubnets: {
+        subnets: privateSubnets.map((subnet) =>
+          cdk.aws_ec2.Subnet.fromSubnetId(
+            this,
+            `PublicSubnetRef-${subnet.node.id}`,
+            subnet.subnetId
+          )
+        ),
+      },
+    });
+
+    service.attachToApplicationTargetGroup(targetGroup);
+
+    new cdk.CfnOutput(this, "VPC_Id", {
+      value: vpc.vpcId,
+    });
   }
 }
